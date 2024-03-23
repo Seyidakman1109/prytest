000122dc8b0bf83745764d991a97afd9f9269c6a
seyid <seyidakman@gmail.com>
2024-03-23 03:26:22 +0300

    test2

--- tests/test_asd.py
+++ tests/test_asd.py
@@ -2,3 +2,14 @@ class TestASD:
     def test_asd(self):
         pass
 
+    def test_asd1(self):
+        pass
+
+    def test_asd2(self):
+        pass
+
+    def test_asd3(self):
+        pass
+
+    def test_asd4(self):
+        pass
